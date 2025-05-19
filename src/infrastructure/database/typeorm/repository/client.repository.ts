60b202bae3474d66e9bab163c3fb958e1779cab7
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2025-05-19 20:45:27 -0300

    feat(repository): adiciona client.repository

---
+++ src/infrastructure/database/typeorm/repository/client.repository.ts
@@ -0,0 +1,42 @@
+import { IClientRepository } from '../../../../core/_share/repository/client.repository.interface';
+import { Client } from '../../../../core/entity/client.entity';
+import { DataSource } from 'typeorm';
+
+export class ClientRepository implements IClientRepository {
+  constructor(private readonly dataSource: DataSource) {}
+
+  async get(id: string): Promise<Client | null> {
+    const result = await this.dataSource.query(
+      'SELECT id, name, email, created_at, updated_at FROM clients WHERE id = ?',
+      [id],
+    );
+
+    if (result.length === 0) {
+      return null;
+    }
+
+    const row = result[0];
+    const client = new Client({
+      id: row.id,
+      name: row.name,
+      email: row.email,
+      createdAt: row.createdAt,
+      updatedAt: row.updatedAt,
+    });
+
+    return client;
+  }
+
+  async save(client: Client): Promise<void> {
+    await this.dataSource.query(
+      'INSERT INTO clients (id, name, email, created_at, updated_at) VALUES (?, ?, ?, ?,?)',
+      [
+        client.value.id,
+        client.value.name,
+        client.value.email,
+        client.value.createdAt,
+        client.value.updatedAt,
+      ],
+    );
+  }
+}
