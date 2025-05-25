fe9b2b160d02e1cc373c3cc0abc8551d850a3e7c
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2025-05-25 06:32:26 -0300

    chore: configura swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './application/dependency-inversion/module/main.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Wallet Core')
+    .setDescription('')
+    .setVersion('1.0')
+    .addTag('client')
+    .addTag('account')
+    .addTag('transaction')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, documentFactory);
+
   await app.listen(3000);
 }
 bootstrap();
