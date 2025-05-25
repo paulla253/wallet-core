ebe67cc4e7530c26f3a51e215826324478093a87
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2025-05-25 06:36:21 -0300

    feat: adiciona funcoes findById e updateBalance

--- src/core/_share/repository/account.repository.interface.ts
+++ src/core/_share/repository/account.repository.interface.ts
@@ -2,4 +2,6 @@ import { Account } from '../../entity/account.entity';
 
 export interface IAccountRepository {
   save(account: Account): Promise<void>;
+  findById(id: string): Promise<Account>;
+  updateBalance(account: Account): Promise<void>;
 }
