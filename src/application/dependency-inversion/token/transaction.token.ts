8bc1a4bf2dc62bab5f786b5d7dd9a5dd848a9255
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2025-06-01 08:27:36 -0300

    feat: adiciona token TransactionUnitOfWordToken

--- src/application/dependency-inversion/token/transaction.token.ts
+++ src/application/dependency-inversion/token/transaction.token.ts
@@ -4,3 +4,7 @@ export const CreateTransactionUseCaseToken: unique symbol = Symbol(
 export const TransactionRepositoryToken: unique symbol = Symbol(
   'TransactionRepositoryToken',
 );
+
+export const TransactionUnitOfWorkToken: unique symbol = Symbol(
+  'TransactionUnitOfWorkToken',
+);
