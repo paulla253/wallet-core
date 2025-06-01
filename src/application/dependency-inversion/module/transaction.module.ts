484840894dd57f2a5e6a9d63c1074b09ed4a4b9c
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2025-06-01 08:30:03 -0300

    feat: melhora transaction module

--- src/application/dependency-inversion/module/transaction.module.ts
+++ src/application/dependency-inversion/module/transaction.module.ts
@@ -1,9 +1,66 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database.module';
 import { TransactionController } from 'src/controller/transaction.controller';
+import { CreateTransactionUseCase } from 'src/core/use-case/create-transaction.use-case';
+import {
+  CreateTransactionUseCaseToken,
+  TransactionRepositoryToken,
+  TransactionUnitOfWorkToken,
+} from '../token/transaction.token';
+import { ITransactionUnitOfWork } from 'src/core/unit-of-work/transaction.uow-interface';
+import { TransactionUnitOfWork } from 'src/infrastructure/unit-of-work/transaction.uow';
+import { QueryRunner } from 'typeorm';
+import { IAccountRepository } from 'src/core/_share/repository/account.repository.interface';
+import { ITransactionRepository } from 'src/core/_share/repository/transaction.repository.interface';
+import { AccountRepository } from 'src/infrastructure/database/typeorm/repository/account.repository';
+import { AccountRepositoryToken } from '../token/account.token';
+import { SQLLiteDataSourceToken } from '../token/database.token';
+import { TransactionRepository } from 'src/infrastructure/database/typeorm/repository/transaction.repository';
 
 @Module({
-  providers: [],
+  providers: [
+    {
+      provide: TransactionRepositoryToken,
+      useFactory: (queryRunner: QueryRunner) => {
+        return new TransactionRepository(queryRunner);
+      },
+      inject: [SQLLiteDataSourceToken],
+    },
+    {
+      provide: AccountRepositoryToken,
+      useFactory: (queryRunner: QueryRunner) => {
+        return new AccountRepository(queryRunner);
+      },
+      inject: [SQLLiteDataSourceToken],
+    },
+    {
+      provide: TransactionUnitOfWorkToken,
+      useFactory: (
+        queryRunner: QueryRunner,
+        accountRepository: IAccountRepository,
+        transactionRepository: ITransactionRepository,
+      ) => {
+        return new TransactionUnitOfWork(
+          queryRunner,
+          accountRepository,
+          transactionRepository,
+        );
+      },
+      inject: [
+        SQLLiteDataSourceToken,
+        AccountRepositoryToken,
+        TransactionRepositoryToken,
+      ],
+    },
+
+    {
+      provide: CreateTransactionUseCaseToken,
+      useFactory: (uow: ITransactionUnitOfWork) => {
+        return new CreateTransactionUseCase(uow);
+      },
+      inject: [TransactionUnitOfWorkToken],
+    },
+  ],
   exports: [],
   imports: [DatabaseModule],
   controllers: [TransactionController],
