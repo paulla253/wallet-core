07f3b52fef564fad9256f4f24818cab2e866675b
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2025-06-21 08:27:51 -0300

    chore: adiciona pacotes e scripts

---
+++ wait-for-mysql.js
@@ -0,0 +1,33 @@
+const readline = require('readline');
+const mysql = require('mysql2');
+
+process.stdout.write(`Initializing MySQL Connection...\n`);
+
+process.on('ECONNREFUSED', () => {
+  console.log('ignore');
+});
+
+function createMysqlConnection() {
+  return mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    port: 3306,
+    database: process.env.MYSQL_DATABASE,
+  });
+}
+
+function tryMysqlConnection() {
+  readline.clearLine(process.stdout, 0);
+  readline.cursorTo(process.stdout, 0);
+
+  const connection = createMysqlConnection();
+  connection.on('error', function (err) {
+    console.log(err);
+    tryMysqlConnection();
+  });
+
+  connection.end();
+}
+
+tryMysqlConnection();
