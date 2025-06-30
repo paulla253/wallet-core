ca5f751a74b31154141d6f62da2e6d512b6401a9
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2025-06-30 20:00:50 -0300

    chore: adiciona jest coverage

---
+++ jest-coverage.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testEnvironment: 'node',
+  testRegex: '.spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    './src/**/*.{ts,js}',
+    '!./src/**/*.spec.{ts,js}',
+    '!./src/main.ts',
+    '!./src/**/dependency-inversion/**/*',
+    '!./src/**/infrastructure/config/**',
+    '!./src/core/**/**.interface.ts',
+    '!./src/infrastructure/event/**',
+    '!./src/infrastructure/unit-of-work/**',
+    '!./src/infrastructure/database/**/migration/*.ts',
+  ],
+
+  coverageThreshold: {
+    global: {
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95,
+    },
+  },
+};
