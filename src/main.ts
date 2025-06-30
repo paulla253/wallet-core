290b0d958cc21410b2551e16a164ea2ca8b1c992
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2025-06-30 07:03:27 -0300

    feat: configura kafka no projeto

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './application/dependency-inversion/module/main.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { KafkaOptions, Transport } from '@nestjs/microservices';
+import KafkaConfiguration from './infrastructure/config/kafka.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configKafka: KafkaOptions = {
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: KafkaConfiguration.CLIENT_ID,
+        brokers: [KafkaConfiguration.BROKER_URL],
+        retry: {
+          retries: 30,
+          initialRetryTime: 10000,
+          maxRetryTime: 5000,
+        },
+      },
+      run: {
+        autoCommit: false,
+      },
+    },
+  };
+
+  app.connectMicroservice(configKafka);
+  await app.startAllMicroservices();
 
   const config = new DocumentBuilder()
     .setTitle('Wallet Core')
