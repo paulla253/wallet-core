e1c73118eeb4011f7c1d3fbede64513a47c3c6cb
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2025-06-30 19:57:02 -0300

    refactor: transaction unit of work

--- src/core/unit-of-work/transaction-uow.interface.ts
+++ src/core/unit-of-work/transaction-uow.interface.ts
@@ -1,10 +1,8 @@
 import { IAccountRepository } from '../_share/repository/account.repository.interface';
 import { ITransactionRepository } from '../_share/repository/transaction.repository.interface';
+import { IUnitOfWork } from './unit-of-work.interface';
 
-export interface ITransactionUnitOfWork {
+export interface ITransactionUnitOfWork extends IUnitOfWork {
   accountRepository: IAccountRepository;
   transactionRepository: ITransactionRepository;
-  start(): Promise<void>;
-  complete(): Promise<void>;
-  rollback(): Promise<void>;
 }
